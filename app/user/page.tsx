43bf83c5bfedb5efa0323ea25d2be7d389801d3b
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2024-02-01 11:53:31 +0700

    move file

---
+++ app/user/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100076110765848%26mibextid%3DZbWKwL&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="500"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+}
+
+export default page;
