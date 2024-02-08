41c8d307478cda66226bc5b41961f1522367e4b6
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2024-02-08 08:39:45 +0700

    Update Dropdown.tsx

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -5,33 +5,31 @@ import React, { useState } from "react";
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMSIYfCJdNMn2ZflsSbPpfXXaTDvqZ5NlWvw&usqp=CAU"
+  const img =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMSIYfCJdNMn2ZflsSbPpfXXaTDvqZ5NlWvw&usqp=CAU";
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  const options = ["Option 1", "Option 2", "Option 3"];
+  const options = ["Profile", "Logout "];
   return (
-      <div className="relative">
-        <button
-          onClick={toggleDropdown}
-          className="bg-blue-500 bg-cover focus:outline-none overflow-hidden w-10 h-10  focus:shadow-outline  rounded-full  "
-        >
-            <img className=" h-auto w-auto " src={img} alt="" />
-          
-        </button>
+    <div className="relative">
+      <button
+        onClick={toggleDropdown}
+        className="bg-blue-500 bg-cover focus:outline-none overflow-hidden w-10 h-10  focus:shadow-outline  rounded-full  "
+      >
+        <img className=" h-auto w-auto " src={img} alt="" />
+      </button>
 
-        {isOpen && (
-          <div className=" p-2 w-fit flex flex-col absolute top-8 mt-2 bg-white border rounded shadow-md text-black">
-            {options.map((option, index) => (
-              <Link key={index} className="p-2  text-nowrap" href="/">
-                {option}
-              </Link>
-            ))}
-          </div>
-        )}
-      </div>
+      {isOpen && (
+        <div className=" p-2 w-fit flex flex-col absolute top-8 mt-2 bg-white border rounded shadow-md text-black">
+          <Link className="p-2  text-nowrap" href="/">
+            {options[0]}
+          </Link>
+        </div>
+      )}
+    </div>
   );
 }
 
