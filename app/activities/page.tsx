ce08dc819209d8834fe8201375072c1b6e5e624c
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2024-02-28 15:14:54 +0700

    12

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -61,7 +61,10 @@ export default function RecipeReviewCard() {
   return (
     <div>
       {post?.map((Attri, Index_num) => (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          key={Index_num}
+          className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
           <img className="rounded-t-lg" src={Attri.img} alt="" />
           <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
