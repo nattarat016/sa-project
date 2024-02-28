0c1ab267b0eccbeb82e8a948c766eff77fd5dde0
kingly5556 <83743078+kingly5556@users.noreply.github.com>
2024-02-28 15:12:09 +0700

    Update page.tsx

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -64,11 +64,9 @@ export default function RecipeReviewCard() {
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <img className="rounded-t-lg" src={Attri.img} alt="" />
           <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {Attri.name}
-              </h5>
-            </a>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {Attri.name}
+            </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
               {Attri.decription}
             </p>
