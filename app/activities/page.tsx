c2d15a222557c95bf619a4370654acd90bb1153f
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2024-02-29 09:27:18 +0700

    รวย

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,35 +1,10 @@
-"use client";
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
+
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { blue, red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 class Fakedata {
   Name: string | undefined;
   Description: string | undefined;
@@ -52,26 +27,32 @@ const post = [
     decription: "เหลืองรักพ่อ",
   },
 ];
-export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
+export default async function RecipeReviewCard() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+  let { data: activities, error } = await supabase
+    .from("activities")
+    .select("*");
+
+  if (error) return redirect("/");
+
+  console.log(activities);
+
   return (
     <div>
-      {post?.map((Attri, Index_num) => (
+      {activities?.map((Attri, index) => (
         <div
-          key={Index_num}
+          key={index}
           className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         >
-          <img className="rounded-t-lg" src={Attri.img} alt="" />
+          <img className="rounded-t-lg" src={Attri.imgurl} alt="" />
           <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {Attri.name}
             </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {Attri.decription}
+              {Attri.description}
             </p>
           </div>
         </div>
