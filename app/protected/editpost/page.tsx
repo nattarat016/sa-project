63ddf8ed3b389313b223e69ad6ba907670a060a0
kingly5556 <kingly5556@gmail.com>
2024-03-06 19:03:47 +0700

    Convert from Delete by id to delete by name of post

--- app/protected/editpost/page.tsx
+++ app/protected/editpost/page.tsx
@@ -11,25 +11,25 @@ export default async function edit({
   searchParams: { message: string };
 }) {
   const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
+  const supabase = createClient(cookieStore);
   const _delete = async (FormData: FormData) => {
     "use server";
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
-    
-    const postId = FormData.get("postId") as unknown as number;
+
+    const postName = FormData.get("postName") as string;
     const { error } = await supabase
       .from("activities")
       .delete()
-      .eq("id", postId);
+      .eq("name", postName);
     console.log(error);
   };
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/login")
+    return redirect("/login");
   }
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center py-10">
@@ -54,13 +54,13 @@ export default async function edit({
         <div>
           <form action={_delete}>
             <label className="text-md" htmlFor="fullname">
-              Fill post ID to Delete
+              Fill post Name to Delete
             </label>
             <br />
             <input
               className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              name="postId"
-              placeholder="ID of post"
+              name="postName"
+              placeholder="Name of post"
               required
             />
             {searchParams?.message && (
